1cd3575622645a17fbf8d102835bba89ebf1cc7b
Chris Pitts <cp25393@gmail.com>
2022-12-26 17:57:47 -0600

    added sliding animation

--- src/script.js
+++ src/script.js
@@ -3,3 +3,18 @@ function onBurgerClick() {
     if (element)
         element.classList.toggle("visible");
 }
+function updateSlidingPositions() {
+    document.querySelectorAll(".slide-container").forEach(function (element) {
+        var elementHeight = element.offsetHeight;
+        var elementPosition = element.getBoundingClientRect().top - window.innerHeight;
+        var percentage = 1 - (-elementPosition) / elementHeight;
+        var translate = percentage < 0 ? 0 : 200 * percentage;
+        console.log(translate);
+        element.querySelector(".slide-right").style.transform = "translateX(-" + translate + "px)";
+        element.querySelector(".slide-up").style.transform = "translateY(" + translate + "px)";
+    });
+}
+addEventListener("scroll", function (event) {
+    updateSlidingPositions();
+});
+updateSlidingPositions();
--- src/script.ts
+++ src/script.ts
@@ -3,3 +3,25 @@ function onBurgerClick() {
     if (element)
         element.classList.toggle("visible");
 }
+
+function updateSlidingPositions(){
+
+
+    document.querySelectorAll(".slide-container").forEach((element)=>{
+        let elementHeight = (element as HTMLElement).offsetHeight;
+        let elementPosition = element.getBoundingClientRect().top - window.innerHeight;
+        let percentage = 1 - (-elementPosition) / elementHeight;
+        let translate = percentage < 0 ? 0 : 200 * percentage
+        
+        console.log(translate);
+        (element.querySelector(".slide-right") as HTMLElement).style.transform = "translateX(-" + translate + "px)"; 
+        (element.querySelector(".slide-up") as HTMLElement).style.transform = "translateY(" + translate + "px)"; 
+    })
+
+}
+
+addEventListener("scroll", (event)=>{
+    updateSlidingPositions();
+})
+
+updateSlidingPositions();
