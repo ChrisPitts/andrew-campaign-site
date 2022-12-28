1dbee7379cb1a7dc890a79b77fb7a02d006eb2f8
Chris Pitts <cp25393@gmail.com>
2022-12-28 09:37:52 -0600

    added animation to issues

--- src/script.js
+++ src/script.js
@@ -5,16 +5,28 @@ function onBurgerClick() {
 }
 function updateSlidingPositions() {
     document.querySelectorAll(".slide-container").forEach(function (element) {
-        var elementHeight = element.offsetHeight;
-        var elementPosition = element.getBoundingClientRect().top - window.innerHeight;
-        var percentage = 1 - (-elementPosition) / elementHeight;
-        var translate = percentage < 0 ? 0 : 200 * percentage;
-        console.log(translate);
-        element.querySelector(".slide-right").style.transform = "translateX(-" + translate + "px)";
-        element.querySelector(".slide-up").style.transform = "translateY(" + translate + "px)";
+        var translate;
+        if (window.innerWidth <= 1200)
+            translate = 0;
+        else {
+            var elementHeight = element.offsetHeight;
+            var elementPosition = element.getBoundingClientRect().top - window.innerHeight;
+            var percentage = 1 - (-elementPosition) / elementHeight;
+            translate = percentage < 0 ? 0 : 200 * percentage;
+        }
+        var slideRight = element.querySelector(".slide-right");
+        var slideLeft = element.querySelector(".slide-left");
+        var slideUp = element.querySelector(".slide-up");
+        if (slideRight)
+            slideRight.style.transform = "translateX(-" + translate + "px)";
+        if (slideLeft)
+            slideLeft.style.transform = "translateX(" + translate + "px)";
+        if (slideUp)
+            slideUp.style.transform = "translateY(" + translate + "px)";
     });
 }
 addEventListener("scroll", function (event) {
     updateSlidingPositions();
 });
+addEventListener("resize", function (event) { return updateSlidingPositions(); });
 updateSlidingPositions();
--- src/script.ts
+++ src/script.ts
@@ -6,16 +6,28 @@ function onBurgerClick() {
 
 function updateSlidingPositions(){
 
-
+    
     document.querySelectorAll(".slide-container").forEach((element)=>{
-        let elementHeight = (element as HTMLElement).offsetHeight;
-        let elementPosition = element.getBoundingClientRect().top - window.innerHeight;
-        let percentage = 1 - (-elementPosition) / elementHeight;
-        let translate = percentage < 0 ? 0 : 200 * percentage
         
-        console.log(translate);
-        (element.querySelector(".slide-right") as HTMLElement).style.transform = "translateX(-" + translate + "px)"; 
-        (element.querySelector(".slide-up") as HTMLElement).style.transform = "translateY(" + translate + "px)"; 
+        let translate:number;
+        if(window.innerWidth <= 1200)
+            translate = 0;
+        else{
+            let elementHeight = (element as HTMLElement).offsetHeight;
+            let elementPosition = element.getBoundingClientRect().top - window.innerHeight;
+            let percentage = 1 - (-elementPosition) / elementHeight;
+            translate = percentage < 0 ? 0 : 200 * percentage
+        }
+        
+        let slideRight = element.querySelector(".slide-right") as HTMLElement;
+        let slideLeft = element.querySelector(".slide-left") as HTMLElement;
+        let slideUp = element.querySelector(".slide-up") as HTMLElement
+        if(slideRight)
+            slideRight.style.transform = "translateX(-" + translate + "px)";
+        if(slideLeft)
+            slideLeft.style.transform = "translateX(" + translate + "px)";
+        if(slideUp)
+            slideUp.style.transform = "translateY(" + translate + "px)"; 
     })
 
 }
@@ -23,5 +35,7 @@ function updateSlidingPositions(){
 addEventListener("scroll", (event)=>{
     updateSlidingPositions();
 })
+
+addEventListener("resize", (event)=>updateSlidingPositions())
 
 updateSlidingPositions();
